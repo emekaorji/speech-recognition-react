ed1a591ed8dba8bd70c68bd7068755526c45ecdd
Emeka Orji <emekapraiseo@gmail.com>
2023-10-11 13:44:17 +0100

    build: install types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 const SpeechRecognition =
